98cadc122db51e365b1689c09fe55795a7edb20a
flavia sigoli <flaviasivm@gmail.com>
2023-10-30 17:09:28 -0300

    animation initial

--- javascript.js
+++ javascript.js
@@ -7,36 +7,20 @@ toggleButton.addEventListener('click', () => {
 })
 
 // scroll behavior
-window.addEventListener("scroll", setScrollVar)
-window.addEventListener("resize", setScrollVar)
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
 
-function setScrollVar() {
-  const htmlElement = document.documentElement
-  const percentOfScreenHeightScrolled =
-    htmlElement.scrollTop / htmlElement.clientHeight
-  console.log(Math.min(percentOfScreenHeightScrolled * 100, 100))
-  htmlElement.style.setProperty(
-    "--scroll",
-    Math.min(percentOfScreenHeightScrolled * 100, 100)
-  )
-}
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
 
-setScrollVar()
-
-const observer = new IntersectionObserver(entries => {
-  for (let i = entries.length - 1; i >= 0; i--) {
-    const entry = entries[i]
-    if (entry.isIntersecting) {
-      document.querySelectorAll("[missiongrid img]").forEach(img => {
-        img.classList.remove("show")
-      })
-      const img = document.querySelector(entry.target.dataset.imgToShow)
-      img?.classList.add("show")
-      break
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
     }
   }
-})
+}
 
-document.querySelectorAll("[data-img-to-show]").forEach(section => {
-  observer.observe(section)
-})
+window.addEventListener("scroll", reveal);
