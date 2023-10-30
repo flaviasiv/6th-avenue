174611b3875d83bf01757d822b099ce78bb6ff00
flavia sigoli <flaviasivm@gmail.com>
2023-10-30 14:08:35 -0300

    font test

--- javascript.js
+++ javascript.js
@@ -5,3 +5,38 @@ const navbarLinks = document.getElementsByClassName('menu-links', 'menusocial')[
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
+
+// scroll behavior
+window.addEventListener("scroll", setScrollVar)
+window.addEventListener("resize", setScrollVar)
+
+function setScrollVar() {
+  const htmlElement = document.documentElement
+  const percentOfScreenHeightScrolled =
+    htmlElement.scrollTop / htmlElement.clientHeight
+  console.log(Math.min(percentOfScreenHeightScrolled * 100, 100))
+  htmlElement.style.setProperty(
+    "--scroll",
+    Math.min(percentOfScreenHeightScrolled * 100, 100)
+  )
+}
+
+setScrollVar()
+
+const observer = new IntersectionObserver(entries => {
+  for (let i = entries.length - 1; i >= 0; i--) {
+    const entry = entries[i]
+    if (entry.isIntersecting) {
+      document.querySelectorAll("[missiongrid img]").forEach(img => {
+        img.classList.remove("show")
+      })
+      const img = document.querySelector(entry.target.dataset.imgToShow)
+      img?.classList.add("show")
+      break
+    }
+  }
+})
+
+document.querySelectorAll("[data-img-to-show]").forEach(section => {
+  observer.observe(section)
+})
