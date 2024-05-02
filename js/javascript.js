c884a38a2287127a531c57aba5627c96344a64f3
flavia sigoli <flaviasivm@gmail.com>
2024-05-02 17:42:50 -0300

    otimizacao1

--- js/javascript.js
+++ js/javascript.js
@@ -15,28 +15,6 @@ document.onclick = function(e){
     }
 }
 
-// // scroll behavior
-// let section = document.querySelectorAll("main");
-// let menu = document.querySelectorAll("header nav a");
-
-// window.onscroll = () => {
-//   section.forEach((i) => {
-//     let top = window.scrollY;
-//     let offset = i.offsetTop - 150;
-//     let height = i.offsetHeight;
-//     let id = i.getAttribute("id");
-
-//     if (top >= offset && top < offset + height) {
-//       menu.forEach((link) => {
-//         link.classList.remove("active");
-//         document
-//           .querySelector("header nav a[href*=" + id + "]", ".testemonycard")
-//           .classList.add("active");
-//       });
-//     }
-//   });
-// };
-
 function reveal() {
   var reveals = document.querySelectorAll(".reveal");
 
